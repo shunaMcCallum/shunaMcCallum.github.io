c5a42dd5062300eeda863b1a24964975c1c354a0
shunaMcCallum <shuna.mccallum@gmail.com>
2022-09-11 22:02:36 +0100

    initial app build completed and app.js file created along with fonts

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
+import HomeContainer from './containers/HomeContainer';
+import NavBar from './components/NavBar';
+import Projects from './containers/Projects';
+import About from './containers/About';
 import './App.css';
+import GymProject from './components/IndividualProjects/GymProject';
+import HelloWorld from './components/IndividualProjects/HelloWorld';
+import MusicElephant from './components/IndividualProjects/MusicElephant';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route exact path="/portfolio" element={<HomeContainer />} />
+          <Route exact path="/portfolio/about" element={<About />} />
+          <Route exact path="/portfolio/projects" element={<Projects />} />
+          <Route exact path="/portfolio/projects/the_gym" element={<GymProject />} />
+          <Route exact path="/portfolio/projects/hello_world" element={<HelloWorld />} />
+          <Route exact path="/portfolio/projects/music_elephant" element={<MusicElephant />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
